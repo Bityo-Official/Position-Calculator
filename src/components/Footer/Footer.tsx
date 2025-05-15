f72d731d4682e2229b78dcc37df98affb72ef781
mmm25002500 <marryhan25648@gmail.com>
2025-05-15 16:29:01 +0800

    feat: update footer year

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-2">
       <div className="container mx-auto text-center">
-        <p>© 2024 幣友 BITYO. ｜ All Rights Reserved.</p>
+        <p>© 2025 幣友 BITYO. ｜ All Rights Reserved.</p>
       </div>
     </footer>
   );
